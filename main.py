03c1311face9c2b928e13be285e55e6d358e0534
Jerome DESMARES <jdesmare@e2r2p5.42.fr>
2017-09-28 15:10:10 +0200

    parser ok

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import argparse
+
+def get_type(string):
+    if not os.path.exists(string) or string[-4:] != '.txt':
+        raise argparse.ArgumentTypeError("{0} is not a valid format or does not exist. Use --help for more informaion".format(string))
+    return open(string, 'r')
 
 def parse_arg(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("<file>", help="the file must be a .txt")
+    parser.add_argument("file", type=get_type, help="the file must be a .txt")
     args = parser.parse_args()
+    return args.file
 
 def main(argv):
-    parse_arg(argv)
+    fd = parse_arg(argv)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
