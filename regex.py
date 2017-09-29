793682774f249b9693821d0248a15504dce9b2b8
Olivier SENG <oseng@e2r2p7.42.fr>
2017-09-29 15:26:21 +0200

    correction regex

--- regex.py
+++ regex.py
@@ -2,19 +2,24 @@ import re
 
 def my_regex(data):
     line = data.read()
+    line = re.sub('#(.+?)\n', '\n', line)
+    line = re.sub('\n+', '\n', line)
+    print (line)
     # lefteq = re.findall('.*[A-Z()!]\s*(?=\=>)|.*[A-Z()!]\s*(?=<\=>)', line.replace(' ', ''))
     # righteq = re.findall('(?=\=>).*[A-Z()!]\s*(?=\#)|(?=\<=>).*[A-Z()!]\s*(?=\#)', line.replace(' ', ''))
     # print(lefteq)
     # print(righteq)
-    eq = re.findall('.*[A-Z()!]\s*(?=\#)|.*[A-Z()!]\s*(?=\#)', line.replace(' ', ''))
-    line = re.sub('#(.+?)\n', '', line)
+    eq = re.findall('.*[A-Z()!]\s*(?=\#)|.*[A-Z()!]\s*(?=\#)|.*[A-Z()!]\s*(?=\n)|.*[A-Z()!]\s*(?=\n)', line.replace(' ', ''))
+    print(eq)
     letters = re.findall('[A-Z]', line)
     letters.sort()
+
     true = re.findall('(?<=\n=).*' ,line)#ne doit pas etre en premiere ligne
+    print(true)
     question = re.findall('(?<=\n\?).*' ,line)#ne doit pas etre en premiere ligne
+    print(question)
 
     lists = [eq, letters, true, question]
     letters = list(set(letters))
 
-    print(lists)
     return lists
