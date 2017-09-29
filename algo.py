c3fbd25ae7b1c648e47e68a61940b26e0b641008
Jerome DESMARES <jdesmare@e2r1p6.42.fr>
2017-09-29 14:54:04 +0200

    go set up

--- algo.py
+++ algo.py
@@ -1,4 +1,4 @@
-
+from parsing import parse_eq
 
 def create_eq_graph(eq, letters):
     parse_eq(eq)
--- parsing.py
+++ parsing.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 
 def parse_arg(argv):
     parser = argparse.ArgumentParser()
@@ -11,3 +12,14 @@ def open_file(string):
     if not os.path.exists(string) or string[-4:] != '.txt':
         raise argparse.ArgumentTypeError("{0} is not a valid format or does not exist. Use --help for more informaion".format(string))
     return open(string, 'r')
+
+def parse_eq(equations):
+    for eq in equations:
+        for char in eq:
+            if is_op(char) and is_op(char - 1):
+                print ("Error : two operators cannot follow each other.")
+                sys.exit()
+            if is_op(char) and (char - 1 is '(' or char + 1 is ')'):
+                print ("Error : bad placement of brackets or {0}")
+def is_op(a):
+    return (a is '+' or a is '^' or a is '|')
