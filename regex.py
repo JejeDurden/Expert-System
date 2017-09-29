48c793a7ca0da281eb0ee9b4af1399f69641a48b
Olivier SENG <oseng@e2r2p7.42.fr>
2017-09-29 14:52:03 +0200

    repush car mauvaise equation / regex

--- regex.py
+++ regex.py
@@ -2,13 +2,12 @@ import re
 
 def my_regex(data):
     line = data.read()
-    line = re.sub('#(.+?)\n', '', line)
-    print (line)
     # lefteq = re.findall('.*[A-Z()!]\s*(?=\=>)|.*[A-Z()!]\s*(?=<\=>)', line.replace(' ', ''))
     # righteq = re.findall('(?=\=>).*[A-Z()!]\s*(?=\#)|(?=\<=>).*[A-Z()!]\s*(?=\#)', line.replace(' ', ''))
     # print(lefteq)
     # print(righteq)
     eq = re.findall('.*[A-Z()!]\s*(?=\#)|.*[A-Z()!]\s*(?=\#)', line.replace(' ', ''))
+    line = re.sub('#(.+?)\n', '', line)
     letters = re.findall('[A-Z]', line)
     letters.sort()
     true = re.findall('(?<=\n=).*' ,line)#ne doit pas etre en premiere ligne
@@ -17,4 +16,5 @@ def my_regex(data):
     lists = [eq, letters, true, question]
     letters = list(set(letters))
 
+    print(lists)
     return lists
